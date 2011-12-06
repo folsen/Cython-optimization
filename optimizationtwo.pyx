47670258cdacc4bb384d4d284a02b57c78a1ab01
Fredrik Olsen <ique@3h.se>
2011-12-06 15:11:20 +0100

    Adding in cython support for Numpy. Adding in more type declarations for all the numpy arrays.

--- optimizationtwo.pyx
+++ optimizationtwo.pyx
@@ -1,6 +1,10 @@
 from __future__ import division
-from scipy.linalg import *
-from numpy import *
+from scipy.linalg import (norm, solve, cholesky, solve_triangular)
+import numpy as np
+cimport numpy as np
+
+DTYPE = np.int
+ctypedef np.int_t DTYPE_T
 
 class OptimizationProblem (object):
   def __init__(self, f, grad=0):
@@ -14,13 +18,15 @@ class OptimizationMethod (object):
     self.linesearch=linesearch
     self.verbose = verbose
 
-  def __call__(self, x):
+  def __call__(self, np.ndarray x):
     # Kor loopen
     cdef int maxit = 100
     cdef double tol = 1e-8
     cdef double alpha 
 
     g = self.prob.g
+
+    cdef np.ndarray H, xold, s
 
     for i in range(0,maxit):
       if i == 0:
@@ -42,14 +48,15 @@ class OptimizationMethod (object):
       xold = x
       x = x-alpha*s
 
-      if norm(array(x)-array(xold)) < tol:
+      if norm(np.array(x)-np.array(xold)) < tol:
         if self.verbose:
           print "Found answer in " + str(i) + " iterations."
         return x
 
     raise Exception("Didn't converge.")
 
-  def chol_solve(self,H,g):
+  def chol_solve(self,np.ndarray H,g):
+    cdef np.ndarray L, lp, Hi
     try:
       L = cholesky(H)
       lp = solve_triangular(L.T, g, lower=True)
@@ -58,15 +65,15 @@ class OptimizationMethod (object):
     except:
       raise Exception("G not pos. def. please pick a guess closer to the target.")
 
-  def initial_h(self, x):
+  def initial_h(self, np.ndarray x):
     cdef double h = 1e-8
     g = self.prob.g
     # Finite difference approximation
-    Gb = array([(g(x + ei * h) - g(x))/h for ei in identity(size(x))])
+    Gb = np.array([(g(x + ei * h) - g(x))/h for ei in np.identity(np.size(x))])
     G = (1./2.) * (Gb + Gb.T)
     return G
 
-  def exact_linesearch(self, x, s, double a_guess):
+  def exact_linesearch(self, np.ndarray x, np.ndarray s, double a_guess):
     #return fmin(self.freeze_function(self.prob.f, x, -s),0,disp=False)
     cdef double a, ll, lu, tol, tmp
 
@@ -93,35 +100,39 @@ class OptimizationMethod (object):
     
     return a
   
-  def freeze_function(self, f, x, s):
+  def freeze_function(self, f, np.ndarray x, np.ndarray s):
+    cdef double ap
     return lambda ap:f(x + ap*s)
 
-  def get_fprime_alpha(self, g, x, s):
-    return lambda a: dot(g(x+a*s),s)
+  def get_fprime_alpha(self, g, np.ndarray x, np.ndarray s):
+    cdef double a
+    return lambda a: np.dot(g(x+a*s),s)
 
 class QuasiNewton (OptimizationMethod):
 
   # def initial_h(self,x):
   #   return identity(size(x))
   
-  def chol_solve(self,H,g):
+  def chol_solve(self,np.ndarray H,g):
     return solve(H,g)  
 
-  def update(self, H, x, xold, double alpha, s):
+  def update(self, np.ndarray H, np.ndarray x, np.ndarray xold, double alpha, s):
     print "Not implemented"
   
 class BFGS (QuasiNewton):
-  def update(self, H, x, xold, double alpha, s):
+  def update(self, np.ndarray H, np.ndarray x, np.ndarray xold, double alpha, np.ndarray s):
+
+    cdef np.ndarray delta, gamma, p1, p2, dH, Hd
 
     g = self.prob.g
     delta = x - xold # alpha*s
     gamma = g(x) - g(xold)
 
-    p1 = outer(gamma,gamma)/inner(gamma,delta)
+    p1 = np.outer(gamma,gamma)/np.inner(gamma,delta)
 
     Hd = H.dot(delta)
     dH = (delta.T).dot(H)
-    p2 = Hd.dot(dH) / inner(dH, delta)
+    p2 = Hd.dot(dH) / np.inner(dH, delta)
 
     #p1 = (1 + (gamma.T).dot(H).dot(gamma)/inner(delta, gamma))*(outer(delta, delta)/inner(delta, gamma))
 
