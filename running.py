a8d2a2ed87a853c0c8cc236535a9309d520e1430
Fredrik Olsen <ique@3h.se>
2011-12-06 15:33:17 +0100

    Adding the running environment and setup file as well in case those change

---
+++ running.py
@@ -0,0 +1,19 @@
+from optimizationtwo import *
+from numpy import *
+from scipy import *
+
+execfile('chebyquad_problem_.py')
+
+x=linspace(0,1,5)
+
+def runtest():
+	prob = OptimizationProblem(chebyquad,gradchebyquad)
+	bfgs = BFGS(prob,linesearch='Exact')
+	return bfgs(x)
+
+if __name__ == '__main__':
+	from timeit import Timer
+	t = Timer("runtest()", "from __main__ import runtest")
+	times = map(lambda x: x/100, t.repeat(repeat=5, number=100))
+	print "Mean runtime in seconds:"
+	print times
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, Extension
+from Cython.Distutils import build_ext
+
+setup ( cmdclass = { 'build_ext': build_ext } ,
+	ext_modules = [
+		Extension("optimizationtwo",
+						["optimizationtwo.pyx"],
+						libraries=["m"])
+		]
+)
